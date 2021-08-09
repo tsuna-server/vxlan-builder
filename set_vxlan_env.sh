dd3df8d03b7471f6883766048057f22560bbdc7c
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2021-08-09 16:32:45 +0900

    Replace vxlan gw name to variable

--- set_vxlan_env.sh
+++ set_vxlan_env.sh
@@ -15,6 +15,9 @@ main() {
 init_env() {
     . "$CONFIG_FILE"
 
+    VXLAN_NAME=${VXLAN_NAME:-vxlan9}
+    VXLAN_GW_NAME=${VXLAN_GW_NAME:-vxlan9gw}
+
     HOST_BRIDGE_IP=$(ip address show dev "$HOST_BRIDGE_INTERFACE" \
             | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" \
             | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
@@ -29,7 +32,7 @@ log_err() {
 set_vxlan() {
 
 
-    ip link add vxlan9 type vxlan id 9 dstport 4789 \
+    ip link add ${VXLAN_NAME} type vxlan id 9 dstport 4789 \
             local ${HOST_BRIDGE_IP} group 239.1.1.1 dev $HOST_BRIDGE_INTERFACE || {
         log_err "Failed to create a name space \"vxlan100\""
         return 1
@@ -40,56 +43,56 @@ set_vxlan() {
         # Create a GW connects VXLAN and outer local network segments.
         set -e
         #brctl addbr br100
-        brctl addif br9 vxlan9
+        brctl addif br9 ${VXLAN_NAME}
         brctl stp br9 off
         #ip link set up dev br100
-        ip link set up dev vxlan9
+        ip link set up dev ${VXLAN_NAME}
 
         ip link add name veth1 type veth peer name veth1-br
-        ip netns add vxlan9gw
+        ip netns add ${VXLAN_GW_NAME}
         brctl addif br9 veth1-br
-        ip link set veth1 netns vxlan9gw
+        ip link set veth1 netns ${VXLAN_GW_NAME}
         ip link add name veth2 type veth peer name veth2-br
-        ip link set veth2 netns vxlan9gw
+        ip link set veth2 netns ${VXLAN_GW_NAME}
         brctl addif br0 veth2-br
 
         ip link set veth1-br up
-        ip netns exec vxlan9gw ip link set veth1 up
+        ip netns exec ${VXLAN_GW_NAME} ip link set veth1 up
         ip link set veth2-br up
-        ip netns exec vxlan9gw ip link set veth2 up
+        ip netns exec ${VXLAN_GW_NAME} ip link set veth2 up
 
-        ip netns exec vxlan9gw sysctl net.ipv4.ip_forward=1
+        ip netns exec ${VXLAN_GW_NAME} sysctl net.ipv4.ip_forward=1
     ) || {
         echo "Failed to create bridges and a namespace." >&2
         return 1
     }
 
     #ip netns exec vxlan100gw ip address add 192.168.2.1/24 dev veth1
-    ip netns exec vxlan9gw ip address add ${VXLAN_GW_INNER_IP} dev veth1 || {
+    ip netns exec ${VXLAN_GW_NAME} ip address add ${VXLAN_GW_INNER_IP} dev veth1 || {
         echo "Failed to set vxlan gw inner IP as ${VXLAN_GW_INNER_IP} on veth1" >&2
         return 1
     }
     #ip netns exec vxlan100gw ip address add 192.168.1.254/24 dev veth2
-    ip netns exec vxlan9gw ip address add ${VXLAN_GW_OUTER_IP} dev veth2 || {
+    ip netns exec ${VXLAN_GW_NAME} ip address add ${VXLAN_GW_OUTER_IP} dev veth2 || {
         echo "Failed to set vxlan gw inner IP as ${VXLAN_GW_OUTER_IP} on veth2" >&2
         return 1
     }
 
-    ip netns exec vxlan9gw ip route add default via ${PRIVODER_GW} || {
-        echo "Failed to set default gw IP ${PROVIDER_GW} of the namespace vxlan9gw." >&2
+    ip netns exec ${VXLAN_GW_NAME} ip route add default via ${PRIVODER_GW} || {
+        echo "Failed to set default gw IP ${PROVIDER_GW} of the namespace ${VXLAN_GW_NAME}." >&2
         return 1
     }
 
     # Set NAT MASQUERADE
     (
         set -e
-        ip netns exec vxlan9gw iptables --table nat --flush
-        ip netns exec vxlan9gw iptables --table nat \
+        ip netns exec ${VXLAN_GW_NAME} iptables --table nat --flush
+        ip netns exec ${VXLAN_GW_NAME} iptables --table nat \
                 --append POSTROUTING --source ${VXLAN_NAT_SOURCE_IP_TO_INTERNET} --jump MASQUERADE
-        ip netns exec vxlan9gw iptables --table nat \
+        ip netns exec ${VXLAN_GW_NAME} iptables --table nat \
                 --append POSTROUTING --source ${VXLAN_NAT_SOURCE_IP_TO_VXLAN} \
                 --destination ${VXLAN_NAT_SOURCE_IP_TO_INTERNET} --jump MASQUERADE
-        ip netns exec vxlan9gw iptables --table nat --list
+        ip netns exec ${VXLAN_GW_NAME} iptables --table nat --list
     ) || {
         echo "Failed to set MASQUERADE of the iptables." >&2
         return 1
